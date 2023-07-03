b0eaa45cb7a0eb589df2d8e6b7e0247902198284
Kathy Hoang <kathyhahoang@gmail.com>
2023-07-03 10:42:55 -0500

    Creates AppLayout component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <footer className="footer">
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorlWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
