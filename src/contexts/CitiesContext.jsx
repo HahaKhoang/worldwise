535e7f41c5cf89c08dc2671586f05eadb908be47
Kathy Hoang <kathyhahoang@gmail.com>
2023-07-05 15:25:08 -0500

    Deletes cities

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -38,6 +38,20 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (error) {
+      alert("There was an error deleting city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   async function createCity(newCity) {
     try {
       setIsLoading(true);
@@ -51,7 +65,7 @@ function CitiesProvider({ children }) {
       const data = await response.json();
       setCities((cities) => [...cities, data]);
     } catch (error) {
-      alert("There was an error loading data...");
+      alert("There was an error creating city...");
     } finally {
       setIsLoading(false);
     }
@@ -59,7 +73,14 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
